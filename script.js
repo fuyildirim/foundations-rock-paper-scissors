6e04eca62ec64761cb9a14cec90d94698a164f86
fuyildirim <yildirim@furkan.no>
2024-04-28 14:11:14 +0200

    final

--- script.js
+++ script.js
@@ -1,1 +1,73 @@
-console.log("Hello World")
+function getComputerChoice () {
+    const choiceArray = ["rock", "paper", "scissors"];
+    const computerChoice = choiceArray[Math.floor(Math.random()*3)]
+    return computerChoice;
+}
+
+function getHumanChoice () {
+    const humanChoice = prompt('Rock, paper, scissors?', "");
+    return humanChoice;
+}
+
+function playRound (humanChoice, computerChoice) {
+    let isWin;
+    let isDraw;
+    humanChoice = humanChoice.toLowerCase();
+    if (humanChoice === "rock") {
+        if (computerChoice === "rock"){
+            isDraw = true;
+        } else if (computerChoice === "paper") {
+            isWin = true;
+            humanScore ++;
+        } else if (computerChoice === "scissors") {
+            isWin = false;
+            computerScore ++;
+        }
+    } else if (humanChoice === "paper") {
+        if (computerChoice === "rock") {
+            isWin = true;
+            humanScore ++;
+        } else if (computerChoice === "paper") {
+            isDraw = true;
+        } else if (computerChoice === "scissors") {
+            isWin = false;
+            computerScore ++;
+        }
+    } else if (humanChoice === "scissors") {
+        if (computerChoice === "rock") {
+            isWin = false;
+            computerScore ++;
+        } else if (computerChoice === "paper") {
+            isWin = true;
+            humanScore ++;
+        } else if (computerChoice === "scissors") {
+            isDraw = true;
+        }
+    }
+    if (isDraw) {
+        return "Draw! " + humanChoice + " vs " + computerChoice;
+    } else {
+        if (isWin) {
+            return "You won! " + humanChoice + " vs " + computerChoice;
+        } else if (!isWin) {
+            return "You lost! " + humanChoice + " vs " + computerChoice;
+        }
+    }
+}
+
+
+function playGame (x){
+    console.log("Spiller " + x + " runder!")
+    for (let i = x; i > 0; i--) {
+        console.log(playRound(getHumanChoice(), getComputerChoice()));
+        console.log("Human score: " + humanScore);
+        console.log("Computer score: " + computerScore)
+    }
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+playGame(5);
+
+
